ae5308a37d28ddb584bd26f321a3660ccf88b2ba
hisayohorie <hisayohorie@gmail.com>
2018-03-17 14:26:53 -0400

    Created shoppig list

--- two.js
+++ two.js
@@ -2,30 +2,43 @@ document.addEventListener('DOMContentLoaded', function() {
 
   var textBox = document.querySelector('#board_size');
   var boardContainer = document.querySelector('.board');
-  // var main = document.querySelector('main');
+  var main = document.querySelector('main');
+  var startButton = document.getElementById('start_button');
+  var form = document.querySelector('form');
 
   textBox.addEventListener('input', reDrawBoard);
+  startButton.addEventListener('click', startGame);
 
   function reDrawBoard(){
-    removeBoard();
     var size = parseInt(textBox.value);
-
+    removeBoard();
+    removeWarning();
+  //  displayWarning(size);
     drawCells(size);
   }
 
 
   function drawCells(size) {
-    for (var i = 0; i < size; i++) {
-      var row = document.createElement('tr');
-      boardContainer.appendChild(row);
+    if(size>=3){
+      for (var i = 0; i < size; i++) {
+        var row = document.createElement('tr');
+        boardContainer.appendChild(row);
 
-      for (var j = 0; j < size; j++) {
-        var cell = document.createElement('td');
-        cell.classList.add("cell");
-        row.appendChild(cell);
+        for (var j = 0; j < size; j++) {
+          var cell = document.createElement('td');
+          cell.classList.add("cell");
+          row.appendChild(cell);
+
+        }
+
+
+
       }
 
+    }else{
+      displayWarning()
     }
+
   }
 
   function removeBoard(){
@@ -38,13 +51,39 @@ document.addEventListener('DOMContentLoaded', function() {
       warning.remove();
     }
   }
-  
+
   function displayWarning(){
     var warning = document.createElement('p');
     warning.classList.add("warning")
     warning.innerText = "Invalid board size";
     main.prepend(warning);
   }
+
+  /*  disable form once start button is clicked */
+
+  function disableForm(){
+    form.style.display = "none"
+
+  }
+
+  function makeCellsClickable() {
+    var cells = document.querySelectorAll(".cell");
+
+    for (var i = 0; i < cells.length; i++) {
+      var cell = cells[i];
+      cell.addEventListener('click', function(){
+        this.innerText="X"
+      });
+    }
+
+  }
+
+  function startGame() {
+    disableForm();
+    makeCellsClickable();
+  }
+
+  /* tic tac toe */
 
 
 });
