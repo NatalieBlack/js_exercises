8b5232fa79823c78824afb5b3a63f163dc7b06e0
NatalieBlack <natalievictoriablack@gmail.com>
2018-03-22 14:20:27 -0400

    Example to illustrate problem with iterating and setting up event listeners that use variables from a larger scope

---
+++ zero.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var topDivs = document.querySelectorAll(".top div");
+  var bottomDivs = document.querySelectorAll(".bottom div");
+  var counter = 0;
+
+  for (var i = 0; i < topDivs.length; i++) {
+    var currentDiv = topDivs[i];
+    currentDiv.addEventListener("click", function() {
+      this.innerText = i;
+    })
+  }
+
+  for (var i = 0; i < bottomDivs.length; i++) {
+    var currentDiv = bottomDivs[i];
+    currentDiv.addEventListener("click", function() {
+      this.innerText = counter;
+      counter++;
+    })
+  }
+})
