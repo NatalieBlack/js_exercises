ac30ec346789b94f0c33d3b1c943120e5608d15d
NatalieBlack <natalievictoriablack@gmail.com>
2018-03-28 11:40:22 -0400

    Add broken painting thing.

---
+++ scope.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var colours = document.querySelectorAll(".colours div");
+  var brushes = document.querySelectorAll(".brushes div");
+  var canvasSquares = document.querySelectorAll(".canvas div");
+  var currentColour;
+  var currentBrush;
+
+  for (var i = 0; i < colours.length; i++) {
+    var colour = colours[i];
+    colour.addEventListener("click", function() {
+      currentColour = window.getComputedStyle(this).backgroundColor;
+    })
+  }
+
+  for (var i = 0; i < brushes.length; i++) {
+    brushes[i].addEventListener("click", function() {
+      currentBrush = this;
+    })
+  }
+
+  for (var i = 0; i < canvasSquares.length; i++) {
+    var square = canvasSquares[i];
+    var blob = document.createElement("div");
+    
+    square.addEventListener("click", function() {
+      blob.classList.add("blob");
+      blob.classList.add(currentBrush.classList[0] + "Brush");
+      this.innerHTML = "";
+      this.appendChild(blob);
+      blob.style.backgroundColor = currentColour;
+    })
+  }
+
+
+
+})
