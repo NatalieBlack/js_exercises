3ebcccb27018b2d5e1e72abab2dd6754a6b0a767
NatalieBlack <natalievictoriablack@gmail.com>
2018-04-01 15:09:10 -0400

    Add another form

--- form.js
+++ form.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
   var submitButton = document.querySelector("input[type=submit]");
+  var inputs = document.querySelectorAll("input");
 
   submitButton.addEventListener("click", function(e) {
     e.preventDefault();
     this.value = "Submitted"
-    var inputs = this.querySelectorAll("input");
     for (var i = 0; i < inputs.length; i++) {
       inputs[i].disabled = true;
     }
