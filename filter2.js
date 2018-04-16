ef75801f763b2b75d0b5666480b6bf25b172f4a3
hisayohorie <hisayohorie@gmail.com>
2018-04-16 11:45:41 -0400

    Added filter

--- filter2.js
+++ filter2.js
@@ -1,13 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
   var divs = document.querySelectorAll("div");
-  var button = document.querySelector("#filter");
-  var input = document.querySelector("input[type=text]");
+  var button = document.getElementById("filter");
+  var button_2 = document.getElementById("filter_2");
+  var input = document.getElementById("max");
+  var input_2 = document.getElementById("min")
+  var divsArray = Array.from(divs);
 
   populateDivs();
 
    button.addEventListener("click", function() {
-     var max = parseInt(input.value);
-     var divsArray = Array.from(divs);
+     var max = parseFloat(input.value);
 
      var tooBig = divsArray.filter(function(div){
        return parseFloat(div.innerText) > max;
@@ -16,6 +18,19 @@ document.addEventListener("DOMContentLoaded", function() {
      for (var i = 0; i < tooBig.length; i++) {
        tooBig[i].classList.add("tooBig");
      }
+   });
+
+   button_2.addEventListener('click', function(){
+     var min = parseFloat(input_2.value);
+     var tooSmall = divsArray.filter(function(div){
+        return parseFloat(div.innerText) < min;
+     });
+
+     for (var i =0; i < tooSmall.length; i++){
+       tooSmall[i].classList.add("tooSmall");
+     }
+
+
    })
 
    function populateDivs(){
